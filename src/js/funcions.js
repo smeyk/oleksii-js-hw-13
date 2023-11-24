3969c16b43a7c2ee78feef6578a9f097d34420da
Oleksii Guberniuk <oleksii_g@hotmail.com>
2023-11-24 12:49:42 +0100

    add second screen

--- src/js/funcions.js
+++ src/js/funcions.js
@@ -1,12 +1,27 @@
+//Function for creating star rating inside recepi cards
+const getRating = (rating) => {
+	const starsTotal = 5;
+	const starPercentage = (rating / starsTotal) * 100;
+	const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
 
+	return `<div class="stars-rating">
+					<span>${rating}</span>
+					<div class="stars-outer">
+						<div style="width: ${starPercentageRounded}" class="stars-inner"></div>
+					</div>
+					<span class="number-rating"></span>
+			</div>`
 
+}
+
+//Function creates slide for ivent courusel
 const createCouruselSlide = (cookImg, dishImg, zoomDish, nameDish, area) => {
 	return `
 	<div class="slide swiper-slide">
 	<div class="slide__img-wrar">
-	<div class="slide-img first" style="background-image: url(${cookImg})"></div>
-	<div class="slide-img second" style="background-image: url(${zoomDish})"></div>
-	<div class="slide-img third" style="background-image: url(${dishImg})"></div>
+		<div class="slide-img first" style="background-image: url(${cookImg})"></div>
+		<div class="slide-img second" style="background-image: url(${zoomDish})"></div>
+		<div class="slide-img third" style="background-image: url(${dishImg})"></div>
 	</div>
 	<div class="slide-text">
 		<h2>${nameDish}</h2>
@@ -15,5 +30,39 @@ const createCouruselSlide = (cookImg, dishImg, zoomDish, nameDish, area) => {
 	</div>
 	`
 }
+//Function creates list of all categories link
+const createRecipeCategoriesBtn = (name) => {
+	return `<li class="left-column__categories-link" id = "${name}">${name}</li>`
+}
+//Create card of popular recepi
+const createPopularRecipeCard = (id, title, description, img) => {
+	return `<div class="popular-recipes__card" id="${id}">
+	<div class="card-img"><img src="${img}" /></div>
+	<div class="card-wrap-text">
+		<div class="card-title"><h3>${title}</h3></div>
+		<div class="card-description">${description}</div>
+	</div>
+	</div>`
+}
+//Create recepi card
+const createRecipeCard = (bgImg, title, description, rating, id) => {
+	return `<div class="recipe-card" id="${id}">
+					<div class="recipe-card-img" style="background-image: url(${bgImg})">
+					<div class="favorite-heart">
+					<i class="fa-thin fa-heart" style="color: #F8F8F8;"></i>
+						</div>
+						<div class="recipe-card-information">
+							<div class="recipe-card-title">${title}</div>
+							<div class="recipe-card-description">${description}</div>
+					<div class="wrap-rate">
+						<div class="recipe-card-rate">${getRating(rating)}</div>
+						<button class="see-recipe">See recipe</button>
+					</div>
+				</div>
+					</div>
+	</div>`
+}
 
-export { createCouruselSlide };
+
+
+export { createCouruselSlide, createRecipeCategoriesBtn, createPopularRecipeCard, createRecipeCard };
--- src/js/tastyTreatsAPI.js
+++ src/js/tastyTreatsAPI.js
@@ -3,7 +3,17 @@ import axios from "axios";
 export default class TastyTreatsAPI {
 	constructor() {
 		this.baseURL = "https://tasty-treats-backend.p.goit.global/api";
+		this.recipes = "https://tasty-treats-backend.p.goit.global/api/recipes"
 		this.events = "events";
+		this.title = "";
+		this.category = "";
+		this.popular = "";
+		this.page = 1;
+		this.perPage = 6;
+		this.limit = 9;
+		this.time = "";
+		this.area = "";
+		this.ingredient = "";
 	}
 	getIvents() {
 		const url = `${this.baseURL}/${this.events}`;
@@ -16,4 +26,62 @@ export default class TastyTreatsAPI {
 				return response.data;
 			})
 	}
+	getAllCategories() {
+		const url = `${this.baseURL}/categories`;
+
+		return axios.get(url)
+			.then(response => {
+				if (response.status !== 200 || response.data.length === 0) {
+					throw new Error(response.data.error);
+				}
+				return response.data;
+			})
+	}
+	getPopularRecipes() {
+		const url = `${this.recipes}/popular`;
+
+		return axios.get(url)
+			.then(response => {
+				if (response.status !== 200 || response.data.length === 0) {
+					throw new Error(response.data.error);
+				}
+				return response.data;
+			})
+	}
+	getFiltredRecepies() {
+		const url = `${this.recipes}?category=${this.category}&page=${this.page}&limit=${this.limit}&time=${this.time}&area=${this.area}&ingredient=${this.ingredient}`;
+
+		return axios.get(url)
+			.then(response => {
+				if (response.status !== 200 || response.data.length === 0) {
+					throw new Error(response.data.error);
+				}
+				return response.data;
+			})
+	}
+	getAllRecepies() {
+		const url = `${this.recipes}?limit=${this.limit}`;
+		// https://tasty-treats-backend.p.goit.global/api/recipes?page=1&limit=9&title=&category=Beef&area=&ingredient=&time=
+		return axios.get(url)
+			.then(response => {
+				if (response.status !== 200 || response.data.length === 0) {
+					throw new Error(response.data.error);
+				}
+				return response.data;
+			})
+	}
+	getCertainCategory(newCategory) {
+		this.category = newCategory;
+
+		const url = `${this.recipes}?page=${this.page}&limit=${this.limit}&category=${this.category}`;
+
+		return axios.get(url)
+			.then(response => {
+				if (response.status !== 200 || response.data.length === 0) {
+					throw new Error(response.data.error);
+				}
+				return response.data;
+			})
+	}
+
 }
